b2f44940c173bc4782c9cb95b3a5f778bcc2745a
FerroDude <pedroferrodude@hotmail.com>
2021-08-17 17:41:39 +0100

    Mostly done, only missing some content

--- app.js
+++ app.js
@@ -1,25 +1,71 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
-app.use((request, response, next) => {
-  console.log('Middleware logged to console');
-  next();
-});
+//register partials to HBS
+hbs.registerPartials(__dirname + '/views/partials');
 
+//set HBS as templating engine
+app.set('view engine', 'hbs');
+
+//tell express where views are located
+app.set('views', __dirname + '/views');
+
+//fetch resources from public folder(images, styles)
 app.use(express.static('public'));
 
+app.locals.title = 'Alex Honnold';
+
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/views/main.html');
+  response.render('home', {
+    title: 'Alex Honnold',
+    name: 'Alex Honnold',
+    location: {
+      country: 'United States of America',
+      city: 'Sacramento',
+      state: 'California'
+    },
+    article: 'this is the / page'
+  });
 });
 
-app.get('/main', (request, response) => {
-  response.sendFile(__dirname + '/views/main.html');
+app.get('/home', (request, response) => {
+  response.render('home', {
+    title: 'Home',
+    name: 'Alex Honnold',
+    location: {
+      country: 'United States of America',
+      city: 'Sacramento',
+      state: 'California'
+    },
+    header: "It's time to know about Alex Honnold!",
+    article: 'Alex is a rock climber, famous for his free solo climbs.',
+    picture: 'images/alex-honnold-cliff.jpg'
+  });
 });
 app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/views/about.html');
+  response.render('about', {
+    title: 'About',
+    title: 'Alex Honnold',
+    name: 'Alex Honnold',
+    location: {
+      country: 'United States of America',
+      city: 'Sacramento',
+      state: 'California'
+    },
+    header: 'A little bit about Alex.',
+    article: 'this is the about page',
+    picture: 'images/alex-honnold-visto-de-cima.jpg'
+  });
 });
 app.get('/work', (request, response) => {
-  response.sendFile(__dirname + '/views/work.html');
+  response.render('work', {
+    title: 'Work',
+    header: "Some of Alex's most famous achievments.",
+    article: 'this is the work page',
+    picture: 'images/alex-honnold-training.jpg'
+  });
 });
 
 app.listen(3020);
